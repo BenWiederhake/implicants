ca39594201f8bec3b5cad9c6040a4a5b42a1fa5b
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-03-19 16:12:27 +0100

    Implement and test "build rank 0" step (sampling)

---
+++ src/bits.rs
@@ -0,0 +1,51 @@
+// implicants – Enumerate (prime) implicants of an arbitrary function
+// Copyright (C) 2017  Ben Wiederhake
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Thin layer of arbitrary bitset implementation.
+//! I want to be able to replace it easily.
+
+extern crate fixedbitset;
+
+use subint::raw::mk_ones;
+
+pub struct Bitset {
+    backing: fixedbitset::FixedBitSet,
+}
+
+impl Bitset {
+    pub fn of(nbits: u32) -> Self {
+        assert!(nbits < 32, "Can only handle at most 31 bits, but tried {} bits", nbits);
+        // I could probably extend that to include 32, but then this would overflow on x86:
+        let len = (1usize << nbits) as usize;
+        Bitset {
+            backing: fixedbitset::FixedBitSet::with_capacity(len),
+        }
+    }
+
+    pub fn set(&mut self, mask: u32) {
+        self.backing.set(mask as usize, true);
+    }
+
+    pub fn is(&self, mask: u32) -> bool {
+        assert!((mask as usize) < self.backing.len(),
+            "Accessed {}, but len is only {}", mask, self.backing.len());
+        self.backing.contains(mask as usize)
+    }
+
+    pub fn is_any(&self) -> bool {
+        self.backing.count_ones(..) > 0
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,73 @@
+// implicants – Enumerate (prime) implicants of an arbitrary function
+// Copyright (C) 2017  Ben Wiederhake
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#![feature(closure_to_fn_coercion)]
+
+extern crate subint;
+mod bits;
+
+use std::collections::HashMap;
+use subint::raw::mk_ones;
+use bits::Bitset;
+
+type ChunkMap = HashMap<u32, Bitset>;
+type SampleFn = fn(u32) -> bool;
+type ReportFn = fn(u32, u32, bool);
+
+struct Context {
+    sampling_fn: SampleFn,
+    report_fn: ReportFn,
+    arity: u32,
+}
+
+fn build_rank_0(ctx: &Context, into: &mut ChunkMap) {
+    assert_eq!(into.len(), 0);
+    let chunk: &mut Bitset = into.entry(0).or_insert(Bitset::of(ctx.arity));
+
+    for i in 0..mk_ones(ctx.arity) {
+        if (ctx.sampling_fn)(i) {
+            chunk.set(i);
+        }
+    }
+}
+
 #[test]
-fn it_works() {
+fn test_build_0() {
+    // Prepare
+    let sample_fn: SampleFn = |a| { (a % 3) == 0 };
+    let false_fn: ReportFn = |a, b, c| { panic!("But there is nothing to report?!"); };
+    let ctx = Context{
+        sampling_fn: sample_fn,
+        report_fn: false_fn,
+        arity: 3,
+    };
+    let mut chunks = HashMap::new();
+
+    // Call under test
+    build_rank_0(&ctx, &mut chunks);
+
+    // Check
+    assert_eq!(1, chunks.len());
+    let c: &Bitset = &chunks[&0];
+    assert_eq!(true, c.is(0));
+    assert_eq!(false, c.is(1));
+    assert_eq!(false, c.is(2));
+    assert_eq!(true, c.is(3));
+    assert_eq!(false, c.is(4));
+    assert_eq!(false, c.is(5));
+    assert_eq!(true, c.is(6));
+    assert_eq!(false, c.is(7));
 }
